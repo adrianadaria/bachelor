dc4f18c1e2098a25199fedd11c6f37b01c29241b
Adriana <[adrianadebowska@gmail.com]>
2017-04-02 15:29:43 +0200

    PHP and React connection - not READ yet

--- src/client/app/view/customer.jsx
+++ src/client/app/view/customer.jsx
@@ -8,7 +8,27 @@ class Customer extends React.Component {
     this.onLike = this.onLike.bind(this);
   }
 
-  
+
+  getInitialState() {
+    return {
+        customers: [],
+    };
+}
+componentDidMount() {
+    this.serverRequest = $.get("get_all_customers.php", function (customers) {
+        this.setState({
+            customers: JSON.parse(customers)
+        });
+    }.bind(this));
+ 
+    $('.page-header h1').text('Create customer');
+}
+
+
+
+componentWillUnmount() {
+    this.serverRequest.abort();
+}
 
   onLike () {
     let newLikesCount = this.state.likesCount + 1;
@@ -19,6 +39,7 @@ class Customer extends React.Component {
   render() {
     return (
       <div>
+        <p>{this.getInitialState}</p>
         Likes : <span>{this.state.likesCount}</span>
         <div><button onClick={this.onLike}>Like Me</button></div>
       </div>
--- src/client/app/view/index.jsx
+++ src/client/app/view/index.jsx
@@ -6,11 +6,16 @@ class App extends React.Component {
 
   render () {
     return <div>
-      <p> Hello Reactdsss!</p>
+      <div class="page-header">
+            <h1>Loading...</h1>
+        </div>
+      <p>{this.props.changeAppMode}</p>
       <p> tdsssjknknjknjknjnjknj!</p>
       <Customer />
     </div>;
   }
 }
+
+
 
 render(<App/>, document.getElementById('app'));
