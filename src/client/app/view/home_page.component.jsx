4421b38ed6910b3ab0a7cb461dd9d6122f5db739
Adriana <[adrianadebowska@gmail.com]>
2017-05-18 13:54:00 +0200

    text in the middle

--- src/client/app/view/home_page.component.jsx
+++ src/client/app/view/home_page.component.jsx
@@ -20,42 +20,54 @@ class HomePageComponent extends React.Component {
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('read')}>
                 <div className="box-product">
-                  <h1>Products</h1>
+                  <div className="box-text">
+                    <h1>Products</h1>
+                  </div>
                 </div>
               </a>
             </div>
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('customer')}>
                 <div className="box-customer">
-                  <h1>Customers</h1>
+                  <div className="box-text">
+                    <h1>Customers</h1>
+                  </div>
                 </div>
               </a>
             </div>
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('invoice')}>
                 <div className="box-invoice">
-                  <h1>Invoices</h1>
+                  <div className="box-text">
+                    <h1>Invoices</h1>
+                  </div>
                 </div>
               </a>
             </div>
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('account')}>
                 <div className="box-account">
-                  <h1>Accounts</h1>
+                  <div className="box-text">
+                    <h1>Accounts</h1>
+                  </div>
                 </div>
               </a>
             </div>
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('group')}>
                 <div className="box-group">
-                  <h1>Groups</h1>
+                  <div className="box-text">
+                    <h1>Groups</h1>
+                  </div>
                 </div>
               </a>
             </div>
             <div>
               <a href='#' onClick={() => this.props.changeAppMode('order')}>
                 <div className="box-order">
-                  <h1>Orders</h1>
+                  <div className="box-text">
+                    <h1>Orders</h1>
+                  </div>
                 </div>
               </a>
             </div>
